b799527ddd3ea0fef0094f5e124a76fecffc2c25
ShrutiRawal <shruti456rawal@gmail.com>
2021-02-07 10:54:49 +0530

    IoT activity initialised

--- Android/app/src/main/java/com/example/sanjeevani/IotActivity.java
+++ Android/app/src/main/java/com/example/sanjeevani/IotActivity.java
@@ -1,14 +1,79 @@
 package com.example.sanjeevani;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import org.w3c.dom.Text;
 
 public class IotActivity extends AppCompatActivity {
+
+    private TextView txtname,valname,txttemp,valtemp;
+    private ProgressBar progress;
+    private Button btnstart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_iot);
+        initialiseWidgets();
+        //fetchName();
+        btnstart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                progress.setVisibility(View.VISIBLE);
+                fetchName();
+            }
+        });
+    }
+
+    private void fetchName() {
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user!=null){
+            final DatabaseReference ref = FirebaseDatabase.getInstance().getReference().child("user").child(user.getUid());
+            ref.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                    progress.setVisibility(View.GONE);
+                    String name = snapshot.child("name").getValue(String.class);
+                    if (name!=null){
+                        valname.setText(name);
+                    }else {
+                        valname.setText("Shruti Rawal");
+                    }
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+                    Toast.makeText(IotActivity.this,"Error",Toast.LENGTH_SHORT).show();
+                }
+            });
+        }else{
+            Toast.makeText(IotActivity.this,"User not found",Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void initialiseWidgets() {
+        txtname = (TextView)findViewById(R.id.textName);
+        valname = (TextView)findViewById(R.id.valName);
+        txttemp = (TextView)findViewById(R.id.texttemp);
+        valtemp =(TextView) findViewById(R.id.valtemp);
+        progress = (ProgressBar)findViewById(R.id.progressBar);
+        progress.setVisibility(View.GONE);
+        btnstart = (Button)findViewById(R.id.btnstart);
     }
 }
